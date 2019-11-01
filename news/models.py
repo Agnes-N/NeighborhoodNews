4dbd3e3db28a93bda38d56fbb5bbe04094805346
agnes <aggyreina@gmail.com>
2019-11-01 09:24:31 +0200

    made changes in model

--- news/models.py
+++ news/models.py
@@ -28,7 +28,31 @@ class Profile(models.Model):
 
 
 class Neighborhood(models.Model):
-    name = models.CharField(max_length = 30)
+    neigborhood_name = models.CharField(max_length = 30)
     location = models.CharField(max_length = 30)
-    occupants = models.CharField(max_length = 30)
+    occupants = models.IntegerField(default = 0, null = True)
     user = models.ForeignKey(User,on_delete = models.CASCADE, null=True)
+
+    @classmethod
+    def create_neigborhood(cls):
+        hoods = cls.objects.all()
+        return hoods
+
+    def delete_neigborhood(self):
+        self.delete()
+
+    @classmethod
+    def update_neigborhood(cls,id,value):
+        cls.objects.filter(id = id).update(neigborhood_name = new_hood)
+
+    @classmethod
+    def get_neigborhood_by_id(cls,id):
+        neigborhood = cls.objects.filter(id = id).all()
+        return neigborhood
+
+    @classmethod
+    def update_occupants(cls,id,value):
+        cls.objects.filter(id = id).update(occupants = new_occupant)
+
+    def __str__(self):
+        return self.neigborhood_name
